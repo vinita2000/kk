8178bb5b38666dbd85cae10480cc4b89465c6751
Vinita <vinitayadavlkw225@gmail.com>
2020-07-10 16:24:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function function1() {
-    alert("Clicked on h4 tag");
-  }
+function function1()
+{
+    var element = document.getElementById("text-1");
+    element.classList.add("active");
+}
+
+function function2()
+{
+    var element = document.getElementById("text-2");
+    element.classList.add("active");
+}
+
+function function3()
+{
+    var element = document.getElementById("text-3");
+    element.classList.add("active");
+}
+
+function function4()
+{
+    var element = document.getElementById("text-4");
+    element.classList.add("active");
+}
+
+function function5()
+{
+    var element = document.getElementById("text-5");
+    element.classList.add("active");
+}
+  
